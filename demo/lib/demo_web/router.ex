19ebd17a18d0590eb2b3d5c6747fce206e03acfb
Justin Tormey <jrtormey@gmail.com>
2025-05-05 11:04:46 -0500

    Generate demo phoenix app
    mix phx.new demo --no-dashboard --no-ecto --no-gettext --no-mailer

---
+++ demo/lib/demo_web/router.ex
@@ -0,0 +1,27 @@
+defmodule DemoWeb.Router do
+  use DemoWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, html: {DemoWeb.Layouts, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", DemoWeb do
+    pipe_through :browser
+
+    get "/", PageController, :home
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", DemoWeb do
+  #   pipe_through :api
+  # end
+end
