dd3de0b72b736b886508dae1b1783bcd7c89e0f8
Justin Tormey <jrtormey@gmail.com>
2025-05-23 15:03:29 -0500

    Improve profile image demo for crop-image

--- demo/lib/demo_web/live/demo_live/crop_image.ex
+++ demo/lib/demo_web/live/demo_live/crop_image.ex
@@ -4,10 +4,16 @@ defmodule DemoWeb.DemoLive.CropImage do
   def render(assigns) do
     ~H"""
     <Layouts.app flash={@flash}>
-      <.test />
+      <.header>
+        Crop Existing Image
+        <:subtitle>
+          Start cropping, then submit to see the result.
+        </:subtitle>
+      </.header>
+
       <div
         id="crop_example"
-        class="flex gap-8"
+        class="flex gap-8 mb-12"
         phx-hook="CropImage"
         data-crop-image-src={~p"/images/phx-hook.png"}
         data-crop-container-id="crop_example_preview"
@@ -32,41 +38,72 @@ defmodule DemoWeb.DemoLive.CropImage do
         </div>
       </div>
 
+      <.header>
+        Crop Image For Upload
+        <:subtitle>
+          Click on the profile image icon to upload & crop.
+        </:subtitle>
+      </.header>
+
       <div
         id="crop_profile"
-        class="flex gap-8"
+        class="flex gap-8 mb-12"
         phx-hook="CropImage"
         data-crop-aspect="square"
         data-crop-file-input-id={@uploads.profile.ref}
         data-crop-container-id="crop_profile_preview"
+        data-on-has-file={JS.exec("data-show", to: "#crop_profile_popup")}
       >
-        <div class="p-3 bg-base-200 rounded-xl">
-          <div
-            id="crop_profile_preview"
-            phx-update="ignore"
-            class="size-64 relative bg-base-100 mb-3 rounded-lg"
+        <div class="relative h-12 p-1 pl-16 pr-10 bg-base-300 rounded-full flex items-center">
+          <img
+            :if={@path_profile}
+            src={@path_profile}
+            class="absolute left-1 size-10 ring-2 ring-primary rounded-full overflow-hidden"
           />
-          <.form for={@form_profile} phx-submit="submit_profile" phx-change="validate">
-            <.inputs_for_crop form={@form_profile} />
-            <label id="profile_label" for={@uploads.profile.ref}>
+          <.form
+            :if={@path_profile == nil}
+            for={@form_profile}
+            class="absolute left-1"
+            phx-submit="submit_profile"
+            phx-change="validate"
+            phx-remove={JS.exec("data-hide", to: "#crop_profile_popup")}
+          >
+            <label id="profile_label" for={@uploads.profile.ref} class="cursor-pointer">
+              <div class="size-10 bg-base-100 rounded-full flex items-center justify-center">
+                <.icon name="hero-user-circle-mini" class="size-8" />
+              </div>
               <.live_file_input upload={@uploads.profile} class="sr-only" />
             </label>
-            <div class="block cropping:hidden">
-              <.button variant="primary" type="button" class="w-full" onclick="profile_label.click()">
-                Upload profile
-              </.button>
-            </div>
-            <div class="hidden cropping:block">
+            <div
+              id="crop_profile_popup"
+              data-show={
+                JS.show(
+                  transition:
+                    {"transition-all ease-out", "opacity-0 scale-95", "opacity-100 scale-100"}
+                )
+              }
+              data-hide={
+                JS.hide(
+                  transition:
+                    {"transition-all ease-out", "opacity-100 scale-100", "opacity-0 scale-95"}
+                )
+              }
+              class="hidden absolute -left-4 top-6 -translate-x-[100%] -translate-y-[50%] p-3 bg-base-200 rounded-xl"
+            >
+              <div
+                id="crop_profile_preview"
+                phx-update="ignore"
+                class="size-64 relative bg-base-100 mb-3 rounded-lg"
+              />
+              <.inputs_for_crop form={@form_profile} type="hidden" />
               <.button variant="primary" type="submit" class="w-full">
-                Crop image
+                Save profile image
               </.button>
             </div>
           </.form>
-        </div>
-        <div :if={@path_profile}>
-          <div class="p-3 bg-base-200 rounded-xl">
-            <img src={@path_profile} class="size-32 rounded-lg overflow-hidden" />
-          </div>
+          <span class="font-semibold">
+            @username
+          </span>
         </div>
       </div>
     </Layouts.app>
@@ -152,31 +189,18 @@ defmodule DemoWeb.DemoLive.CropImage do
   ## Components
 
   attr :form, Phoenix.HTML.Form, required: true
+  attr :type, :string, default: "number"
 
   def inputs_for_crop(assigns) do
     ~H"""
     <.inputs_for :let={f_crop} field={@form[:crop]}>
-      <div class="hidden cropping:block">
-        <.input data-crop-field="left" type="number" field={f_crop[:left]} readonly label="Left" />
-        <.input data-crop-field="top" type="number" field={f_crop[:top]} readonly label="Top" />
-        <.input data-crop-field="width" type="number" field={f_crop[:width]} readonly label="Width" />
-        <.input
-          data-crop-field="height"
-          type="number"
-          field={f_crop[:height]}
-          readonly
-          label="Height"
-        />
+      <div class={["hidden", if(@type != "hidden", do: "cropping:block")]}>
+        <.input data-crop-field="left" type={@type} field={f_crop[:left]} readonly label="Left" />
+        <.input data-crop-field="top" type={@type} field={f_crop[:top]} readonly label="Top" />
+        <.input data-crop-field="width" type={@type} field={f_crop[:width]} readonly label="Width" />
+        <.input data-crop-field="height" type={@type} field={f_crop[:height]} readonly label="Height" />
       </div>
     </.inputs_for>
-    """
-  end
-
-  attr :x, :string, values: [nil, "square"], default: nil
-
-  def test(assigns) do
-    ~H"""
-    {@x}
     """
   end
 end
